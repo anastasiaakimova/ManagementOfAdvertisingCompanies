7a4c245b56f3df345b83439afbd5fbc35b6840e5
Anastasia Akimova <anastasiyaaki171@gmail.com>
2021-10-17 17:14:50 +0300

    Write exceptions for user

--- src/main/java/by/akimova/ManagementOfAdvertisingCompanies/exception/EntityNotFoundException.java
+++ src/main/java/by/akimova/ManagementOfAdvertisingCompanies/exception/EntityNotFoundException.java
@@ -1,2 +1,13 @@
-package by.akimova.ManagementOfAdvertisingCompanies.exception;public class EntityNotFoundException {
+package by.akimova.ManagementOfAdvertisingCompanies.exception;
+
+/**
+ * This is exception class for catching empty entity.
+ *
+ * @author anastasiyaakimava
+ * @version 1.0
+ */
+public class EntityNotFoundException extends Exception {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
 }
--- src/main/java/by/akimova/ManagementOfAdvertisingCompanies/exception/NotFreeUsernameException.java
+++ src/main/java/by/akimova/ManagementOfAdvertisingCompanies/exception/NotFreeUsernameException.java
@@ -1,2 +1,13 @@
-package by.akimova.ManagementOfAdvertisingCompanies.exception;public class NotFreeUsernameException {
+package by.akimova.ManagementOfAdvertisingCompanies.exception;
+
+/**
+ * This is exception class for catching users who use existing mail in the database.
+ *
+ * @author anastasiyaakimava
+ * @version 1.0
+ */
+public class NotFreeUsernameException extends Exception {
+    public NotFreeUsernameException(String message) {
+        super(message);
+    }
 }
