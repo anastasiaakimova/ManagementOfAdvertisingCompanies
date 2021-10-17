6e81a1f8b3f82efb229e2938f787c5a7615d0a17
Anastasia Akimova <anastasiyaaki171@gmail.com>
2021-10-17 15:28:50 +0300

    Init commit

---
+++ src/main/java/by/akimova/ManagementOfAdvertisingCompanies/ManagementOfAdvertisingCompaniesApplication.java
@@ -0,0 +1,13 @@
+package by.akimova.ManagementOfAdvertisingCompanies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementOfAdvertisingCompaniesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementOfAdvertisingCompaniesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/akimova/ManagementOfAdvertisingCompanies/ManagementOfAdvertisingCompaniesApplicationTests.java
@@ -0,0 +1,13 @@
+package by.akimova.ManagementOfAdvertisingCompanies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementOfAdvertisingCompaniesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
